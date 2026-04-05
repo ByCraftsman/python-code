e8cc08e5ffa99b82d581d03177f0df19a620137d
ByCraftsman <ByCraftsman@users.noreply.github.com>
2026-04-05 15:32:21 +0900

    Add duplicate check query for kor_value

--- Generate_Value_Factors.sql
+++ Generate_Value_Factors.sql
@@ -11,3 +11,10 @@ primary key (종목코드, 기준일, 지표)
 
 select * from kor_value
 where 종목코드 = '005930';
+
+-- 중복 검증
+select 종목코드, 기준일, 지표, count(*) as cnt
+from kor_value
+where 종목코드 = '005930'
+group by 종목코드, 기준일, 지표
+having count(*) > 1;
